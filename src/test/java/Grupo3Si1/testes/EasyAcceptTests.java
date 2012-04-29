333d109784671be997d1a349f5dc84f87cfa9696
jeymisson <jeymissoncz@gmail.com>
2012-04-29 16:50:09 -0300

    voltei 8 HEAD's e adicionei os novos testes

--- src/test/java/Grupo3Si1/testes/EasyAcceptTests.java
+++ src/test/java/Grupo3Si1/testes/EasyAcceptTests.java
@@ -18,18 +18,14 @@ public class EasyAcceptTests extends TestCase {
 
 		List<String> files = new ArrayList<String>();
 		// Put the us1.txt file into the "test scripts" list
-		files.add("/Users/thiagoalmeida/git/SigaBemCaroneiro/src/test/resources/Grupo3Si1/US01.txt");
-		files.add("/Users/thiagoalmeida/git/SigaBemCaroneiro/src/test/resources/Grupo3Si1/US02.txt");
-		files.add("/Users/thiagoalmeida/git/SigaBemCaroneiro/src/test/resources/Grupo3Si1/US03.txt");
-		files.add("/Users/thiagoalmeida/git/SigaBemCaroneiro/src/test/resources/Grupo3Si1/US04.txt");
-		files.add("/Users/thiagoalmeida/git/SigaBemCaroneiro/src/test/resources/Grupo3Si1/US05.txt");
-		files.add("/Users/thiagoalmeida/git/SigaBemCaroneiro/src/test/resources/Grupo3Si1/US06.txt");
-		files.add("/Users/thiagoalmeida/git/SigaBemCaroneiro/src/test/resources/Grupo3Si1/US07.txt");
-		files.add("/Users/thiagoalmeida/git/SigaBemCaroneiro/src/test/resources/Grupo3Si1/US08.txt");
-		files.add("/Users/thiagoalmeida/git/SigaBemCaroneiro/src/test/resources/Grupo3Si1/US09.txt");
-		files.add("/Users/thiagoalmeida/git/SigaBemCaroneiro/src/test/resources/Grupo3Si1/US10.txt");
-		files.add("/Users/thiagoalmeida/git/SigaBemCaroneiro/src/test/resources/Grupo3Si1/US11.txt");
-		files.add("/Users/thiagoalmeida/git/SigaBemCaroneiro/src/test/resources/Grupo3Si1/US12.txt");
+		files.add("src/test/resources/Grupo3Si1/US01.txt");
+		files.add("src/test/resources/Grupo3Si1/US02.txt");
+		files.add("src/test/resources/Grupo3Si1/US03.txt");
+		files.add("src/test/resources/Grupo3Si1/US04.txt");
+		files.add("src/test/resources/Grupo3Si1/US05.txt");
+		files.add("src/test/resources/Grupo3Si1/US06.txt");
+		files.add("src/test/resources/Grupo3Si1/US07.txt");
+		files.add("src/test/resources/Grupo3Si1/US08.txt");
 
 		// Instantiate the Monopoly Game facade
 		SigaBemController control = new SigaBemController();
