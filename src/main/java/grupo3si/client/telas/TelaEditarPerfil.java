abca504ec546ee7198022cf7bbc83c55a5e16d24
Lucas <lucas.ufcg@gmail.com>
2012-05-25 00:45:26 -0300

    interface

---
+++ src/main/java/grupo3si/client/telas/TelaEditarPerfil.java
@@ -0,0 +1,114 @@
+package grupo3si.client.telas;
+
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.FileUpload;
+import com.google.gwt.user.client.ui.InlineLabel;
+import com.google.gwt.user.client.ui.IntegerBox;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.ValueListBox;
+import com.google.gwt.text.client.IntegerRenderer;
+import com.google.gwt.user.client.ui.RadioButton;
+
+public class TelaEditarPerfil extends Composite {
+
+	public TelaEditarPerfil() {
+		
+		AbsolutePanel PainelEditarPerfil = new AbsolutePanel();
+		initWidget(PainelEditarPerfil);
+		PainelEditarPerfil.setSize("1024px", "720px");
+		
+		Image imagePerfil = new Image((String) null);
+		PainelEditarPerfil.add(imagePerfil, 45, 52);
+		imagePerfil.setSize("125px", "146px");
+		
+		FileUpload fileUpload = new FileUpload();
+		PainelEditarPerfil.add(fileUpload, 45, 214);
+		fileUpload.setSize("196px", "18px");
+		
+		InlineLabel nlnlblNome = new InlineLabel("Nome");
+		PainelEditarPerfil.add(nlnlblNome, 330, 52);
+		nlnlblNome.setSize("38px", "18px");
+		
+		IntegerBox nome = new IntegerBox();
+		PainelEditarPerfil.add(nome, 330, 76);
+		nome.setSize("332px", "13px");
+		
+		InlineLabel nlnlblEmail = new InlineLabel("Email");
+		PainelEditarPerfil.add(nlnlblEmail, 330, 230);
+		
+		IntegerBox email = new IntegerBox();
+		PainelEditarPerfil.add(email, 330, 251);
+		email.setSize("332px", "13px");
+		
+		InlineLabel nlnlblLogin = new InlineLabel("Cidade onde mora");
+		PainelEditarPerfil.add(nlnlblLogin, 331, 289);
+		
+		IntegerBox cidadeOndeMora = new IntegerBox();
+		PainelEditarPerfil.add(cidadeOndeMora, 330, 310);
+		cidadeOndeMora.setSize("332px", "13px");
+		
+		InlineLabel nlnlblEstado = new InlineLabel("Estado");
+		PainelEditarPerfil.add(nlnlblEstado, 330, 413);
+		
+		ListBox comboBoxEstados = new ListBox();
+		PainelEditarPerfil.add(comboBoxEstados, 330, 434);
+		comboBoxEstados.setSize("169px", "18px");
+		comboBoxEstados.addItem("Acre - AC");
+		comboBoxEstados.addItem("Alagoas - AL");
+		comboBoxEstados.addItem("Amazonas - AM");
+		comboBoxEstados.addItem("Bahia  - BA");
+		comboBoxEstados.addItem("Cear‡ - CE");
+		comboBoxEstados.addItem("Distrito Federal  - DF");
+		comboBoxEstados.addItem("Esp’rito Santo - ES");
+		comboBoxEstados.addItem("Goi‡s - GO");
+		comboBoxEstados.addItem("Mato Grosso - MT");
+		comboBoxEstados.addItem("Mato Grosso do Sul - MS");
+		comboBoxEstados.addItem("Minas Gerais - MG");
+		comboBoxEstados.addItem("Par‡ - PA");
+		comboBoxEstados.addItem("Para’ba - PB");
+		comboBoxEstados.addItem("Paran‡ - PR");
+		comboBoxEstados.addItem("Pernambuco - PE");
+		comboBoxEstados.addItem("Piau’ - PI");
+		comboBoxEstados.addItem("Rio de Janeiro - RJ");
+		comboBoxEstados.addItem("Rio Grande do Norte - RN");
+		comboBoxEstados.addItem("Roraima - RR");
+		comboBoxEstados.addItem("Santa Catarina - SC");
+		comboBoxEstados.addItem("S‹o Paulo - SP");
+		comboBoxEstados.addItem("Sergipe - SE");
+		comboBoxEstados.addItem("Tocantins - TO");
+		
+		InlineLabel nlnlblEndereo = new InlineLabel("Endere\u00E7o");
+		PainelEditarPerfil.add(nlnlblEndereo, 331, 350);
+		nlnlblEndereo.setSize("105px", "15px");
+		
+		IntegerBox endereco = new IntegerBox();
+		PainelEditarPerfil.add(endereco, 330, 371);
+		endereco.setSize("332px", "13px");
+		
+		InlineLabel nlnlblSexo = new InlineLabel("Sexo");
+		PainelEditarPerfil.add(nlnlblSexo, 330, 111);
+		nlnlblSexo.setSize("38px", "18px");
+		
+		RadioButton sexoMasculino = new RadioButton("new name", " Masculino");
+		PainelEditarPerfil.add(sexoMasculino, 331, 135);
+		
+		RadioButton sexoFeminino = new RadioButton("new name", " Feminino");
+		sexoFeminino.setValue(true);
+		PainelEditarPerfil.add(sexoFeminino, 431, 135);
+		
+		InlineLabel nlnlblDataDeNascimento = new InlineLabel("Data de nascimento    (dd/mm/aaaa)");
+		PainelEditarPerfil.add(nlnlblDataDeNascimento, 330, 169);
+		nlnlblDataDeNascimento.setSize("270px", "18px");
+		
+		IntegerBox dataNasciemento = new IntegerBox();
+		PainelEditarPerfil.add(dataNasciemento, 330, 193);
+		dataNasciemento.setSize("332px", "13px");
+		
+		
+		
+		
+		
+	}
+}
--- src/main/java/grupo3si/client/telas/TelaLogado.java
+++ src/main/java/grupo3si/client/telas/TelaLogado.java
@@ -4,30 +4,72 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Hyperlink;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.event.logical.shared.AttachEvent.Handler;
 import com.google.gwt.event.logical.shared.AttachEvent;
 import com.google.gwt.widget.client.TextButton;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Tree;
+import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.TreeItem;
 
 public class TelaLogado extends Composite {
 
 	public TelaLogado() {
 		
-		AbsolutePanel absolutePanel = new AbsolutePanel();
-		initWidget(absolutePanel);
-		absolutePanel.setSize("1024px", "720px");
+		AbsolutePanel PainelLogado = new AbsolutePanel();
+		initWidget(PainelLogado);
+		PainelLogado.setSize("1024px", "720px");
 		
 		Label lblBemVindo = new Label("Bem Vindo");
-		absolutePanel.add(lblBemVindo, 60, 10);
+		PainelLogado.add(lblBemVindo, 60, 10);
 		
 		TextButton txtbtnSair = new TextButton("Sair");
 		txtbtnSair.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
 				
+				
 			}
 		});
-		absolutePanel.add(txtbtnSair, 914, 10);
+		PainelLogado.add(txtbtnSair, 914, 10);
+		
+		Image fotoPerfil = new Image("imagens/fEIA.jpg");
+		PainelLogado.add(fotoPerfil, 24, 41);
+		fotoPerfil.setSize("134px", "150px");
+		
+		Hyperlink hprlnkEditarPerfil = new Hyperlink("Editar Perfil", false, "newHistoryToken");
+		hprlnkEditarPerfil.addAttachHandler(new Handler() {
+			public void onAttachOrDetach(AttachEvent event) {
+				System.out.println("entrou no evento click");
+				TelaEditarPerfil editarperfil = new TelaEditarPerfil();
+				editarperfil.setVisible(true);
+				RootPanel.get("centro").clear();
+				RootPanel.get("centro").add(editarperfil);
+				
+			}
+		});
+		PainelLogado.add(hprlnkEditarPerfil, 24, 231);
+		
+		Hyperlink hprlnkCadastrarCarona = new Hyperlink("Cadastrar Carona", false, "newHistoryToken");
+		PainelLogado.add(hprlnkCadastrarCarona, 23, 252);
+		
+		TextArea TextAreaStatus = new TextArea();
+		PainelLogado.add(TextAreaStatus, 283, 41);
+		TextAreaStatus.setSize("453px", "39px");
+		
+		TextButton PublicarStatus = new TextButton("Publicar");
+		PainelLogado.add(PublicarStatus, 672, 93);
+		
+		Tree treeHistorico = new Tree();
+		PainelLogado.add(treeHistorico, 24, 280);
+		treeHistorico.setSize("100px", "100px");
+		
+		TreeItem trtmHistrico = new TreeItem("Hist\u00F3rico");
+		treeHistorico.addItem(trtmHistrico);
+		
+		
 		
 	}
 }
